719993e25cff3abf4ac7c6a441090594ab4647af
Todor Mitovski <harvaru@gmail.com>
2015-03-04 02:01:10 +0200

    Adding hard coded test data

---
+++ www/js/fixedData.js
@@ -0,0 +1,47 @@
+var fixedBookData = [{
+        "title" : "Night Watch",
+        "author" : "Sergey Lukianenko",
+    }, {
+        "title" : "Day Watch",
+        "author" : "Sergey Lukianenko",
+    }, {
+        "title" : "The Hobbit",
+        "author" : "J.R.R. Tolkien",
+    }, {
+        "title" : "The Lord of the Rings",
+        "author" : "J.R.R. Tolkien",
+    }, {
+        "title" : "Necronomicon",
+        "author" : "H. P. Lovecraft",
+    }, {
+        "title" : "Mountains of Madness",
+        "author" : "H. P. Lovecraft",
+    }, {
+        "title" : "Sin City",
+        "author" : "Frank Miller",
+    }, {
+        "title" : "Fear and Loathing in Las Vegas",
+        "author" : "Hunter S. Thompson",
+    }, {
+        "title" : "Fragile Things",
+        "author" : "Neil Gaiman",
+    }, {
+        "title" : "Smoke and Mirrors",
+        "author" : "Neil Gaiman",
+    }, {
+        "title" : "American Gods",
+        "author" : "Neil Gaiman",
+    }, {
+        "title" : "The Green Mile",
+        "author" : "Stephen King",
+    }, {
+        "title" : "Night Watch",
+        "author" : "Terry Pratchett",
+    }, {
+        "title" : "The Color of Magic",
+        "author" : "Terry Pratchett",
+    }, {
+        "title" : "Good Omens",
+        "author" : "Terry Pratchett",
+    }
+];
